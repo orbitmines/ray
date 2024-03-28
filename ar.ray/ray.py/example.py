cf26d73fcac6d16e7ce05bbf9b6227bc3c5ee17f
Fadi Shawki <fadiorbitmines@gmail.com>
2024-03-28 15:46:23 +0100

    Some thinking on traversing implementation

--- ar.ray/ray.py/example.py
+++ ar.ray/ray.py/example.py
@@ -74,3 +74,27 @@ Ray.compile(lambda ray: ray)
 # TODO: Message: "Cannot run, need more implementations, for example these:"
 
 # TODO: Could just randomly start generating possible combinations of logical gates (or any methods on Ray), which if the runtime has an explanation for (or in the debug mode just to listen), you can hook into that and create an implementation. In the case of python made something along the lines of an interpreter which gives you one, asks to write one in python...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

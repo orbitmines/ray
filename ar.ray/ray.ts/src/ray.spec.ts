6afe3d200d124507fe27ac279d6235c08d629662
Fadi Shawki <fadiorbitmines@gmail.com>
2024-06-05 10:59:16 +0200

    Load some older TS implementation

---
+++ ar.ray/ray.ts/src/ray.spec.ts
@@ -0,0 +1,22 @@
+import Ray from "./ray";
+
+describe("ray", () => {
+  test(".traverse", () => {
+    let ray = new Ray(1, 2, 3);
+    console.log(ray)
+    console.log(ray())
+
+    // const events: any[] = [];
+    // const ray = Ray.array([]);
+    //
+    // if (ray) {
+    //   ray.is_orbit();
+    //
+    // }
+    // new ray.initial;
+    // ray.initial = new ray();
+    // ray.initial = ray.terminal();
+    // ray.initial = (self): Ray.Any => {}
+    // ray.initial = Ray.Function.Self.Impl((self) => {})
+  });
+})

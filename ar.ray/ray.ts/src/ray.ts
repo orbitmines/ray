30b5981a741d17a429bb50719b76098b1e684024
Fadi Shawki <fadi.shawki@orbitmines.com>
2024-06-06 13:21:44 +0200

    More moving before attempting a final implementation with extremes

--- ar.ray/ray.ts/src/ray.ts
+++ ar.ray/ray.ts/src/ray.ts
@@ -64,24 +64,34 @@ const __ray__ = () => {
 
 const Ray = __ray__()
 
+// TODO: What is the minimal setup to compose/equivalence ONLY
+
 Ray.initial = Ray.none; Ray.self = Ray.none; Ray.terminal = Ray.none
 
-// This {1 -> self/self.self , & 2 -> a, b} could be generalized
+// This {1 -> self/self.self , & 2 -> a, b} could be generalized (is_none, is_orbit, ..)
 Ray.is_none = (self: Any) => self.is_orbit(self.self)
 Ray.is_orbit = (self: Any, other: Any) => self === other
+// TODO: These likely change or merge, generalize. ; They are "is_" -> Does there exist a connection between their `.self`'s ; or basically is there an orbit (so bi-directional), and one-way would be?
+Ray.compose = (a: Any, b: Any) => a.terminal.equivalent(b.initial)
+// TODO: .is_equivalent & is_orbit are 0, 1, n perspective?
+Ray.equivalent = (a: Any, b: Any) => a.self.compose(b.self)
+
 
 Ray.is_initial = (self: Any) => self.initial.is_none
-/* */ Ray.as_initial = (self: Any) => Ray.initial({ self })
 Ray.is_terminal = (self: Any) => self.terminal.is_none
-/* */ Ray.as_terminal = (self: Any) => Ray.terminal({ self })
 Ray.is_vertex = (self: Any) => self.is_initial.nor(self.is_terminal)
-/* */ Ray.as_vertex = (self: Any) => Ray.vertex({ self })
 Ray.is_reference = (self: Any) => self.is_initial.and(self.is_terminal)
-/* */ Ray.as_reference = (self: Any) => Ray.reference({ self })
 Ray.is_boundary = (self: Any) => self.is_initial.xor(self.is_terminal)
-Ray.is_extreme = (self: Any) => self.self.is_none.and(self.is_boundary)
-Ray.is_wall = (self: Any) => self.self.is_none.and(self.initial.is_some).and(self.terminal.is_some)
-Ray.reverse = (self: Any) => new self({ initial: self.terminal, self, terminal: self.initial })
+
+// Ray.is_extreme = (self: Any) => self.self.is_none.and(self.is_boundary)
+// Ray.is_wall = (self: Any) => self.self.is_none.and(self.initial.is_some).and(self.terminal.is_some)
+// Ray.reverse = (self: Any) => new self({ initial: self.terminal, self, terminal: self.initial })
+
+// // TODO { self }, for .reference.as_initial, if it's just .as_initial, we need to break it apart
+// /* */ Ray.as_initial = (self: Any) => Ray.initial({ self })
+// /* */ Ray.as_vertex = (self: Any) => Ray.vertex({ self })
+// /* */ Ray.as_terminal = (self: Any) => Ray.terminal({ self })
+// /* */ Ray.as_reference = (self: Any) => Ray.reference({ self })
 
 // Ray.initial.reverse = Ray.terminal
 // Ray.none.reverse = Ray.some

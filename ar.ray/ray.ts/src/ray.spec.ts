e5213e611efaf85db852468327e86ce56b32270d
Fadi Shawki <fadi.shawki@orbitmines.com>
2024-06-06 11:45:45 +0200

    Move some python thinking into js impl

--- ar.ray/ray.ts/src/ray.spec.ts
+++ ar.ray/ray.ts/src/ray.spec.ts
@@ -3,23 +3,7 @@ import Ray from "./ray";
 describe("ray", () => {
   test(".traverse", () => {
     let ray = new Ray(1, 2, 3);
-    console.log('1', ray)
-    console.log('3', ray.terminal)
-    console.log('2', ray())
-
-
-    // const events: any[] = [];
-    // const ray = Ray.array([]);
-    //
-    // if (ray) {
-    //   ray.is_orbit();
-    //
-    // }
-    // new ray.initial;
-    // ray.initial = new ray();
-    // ray.initial = ray.terminal();
-    // ray.initial = (self): Ray.Any => {}
-    // ray.initial = Ray.Function.Self.Impl((self) => {})
+    ray()
 
     if ('all' in ray)
       delete ray.all

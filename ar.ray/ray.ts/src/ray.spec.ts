c97d54232bbacc301273b0680227028d39343eb0
Fadi Shawki <fadi.shawki@orbitmines.com>
2024-06-08 21:58:09 +0200

    Move better __debug__ method on Ray

--- ar.ray/ray.ts/src/ray.spec.ts
+++ ar.ray/ray.ts/src/ray.spec.ts
@@ -24,63 +24,44 @@
 //   }
 //
 
-import {__ray__} from "./ray";
+import {__ray__, is_function} from "./ray";
 
-const __DEBUG__ = (__RAY__ = __ray__()) => {
+const __DEBUG__ = __ray__().__debug__(
+  (name, method) => {
+    if (name.startsWith('__')) return method
 
-
-
-  const __has__ = __RAY__.__has__;
-  const __delete__ = __RAY__.__delete__;
-  const __set__ = __RAY__.__set__;
-  const __get__ = __RAY__.__get__;
-  const __call__ = __RAY__.__call__;
-
-  __RAY__.__has__ = (property: string | symbol): boolean => {
-    console.log('__has__', property)
-    return __has__(property)
+    return (...args: any) => {
+      console.log(name, method, args)
+      return is_function(method) ? method(...args) : (method as any).proxy[name]
+    };
   }
-  __RAY__.__delete__ = (property: string | symbol): boolean => {
-    console.log('__delete__', property)
-    return __delete__(property)
-  }
-  __RAY__.__get__ = (property: string | symbol): any => {
-    console.log('__get__', property)
+);
 
-    // if (String(property) === '$$typeof') return 'Ray';
-    // if (String(property) === 'constructor') return this;
-    // if (String(property) === '@@__IMMUTABLE_ITERABLE__@@') return Ray.prototype
+// Set all the methods defined on `Ray` through `__set__`. As if we used `Ray.something = something`
+__DEBUG__.__methods__
+  .filter(name => !name.startsWith('__'))
+  .forEach(method => __DEBUG__.__set__(method, __DEBUG__.__method__(method)));
 
-    return __get__(property);
-  }
-  __RAY__.__set__ = (property: string | symbol, value: any): boolean => {
-    console.log('__set__', property, value)
-    return __set__(property, value);
-  }
-  __RAY__.__call__ = (args: any[] = []): any => {
-    console.log('__call__', args)
-    return __call__(args);
-  }
-
-  return __RAY__;
-}
-
-const Ray = __ray__().__debug__(
-  (name, method, args) => {
-    console.log(name, args)
-    return method;
-  }
-).proxy;
-
+const Ray = __DEBUG__.proxy
 
 describe("ray", () => {
   test("Minimal setup", () => {
     // expect(Ray.none.is_none()).toBe(undefined)
 
-    // // We cannot see the difference between any definition of `.none`.
+    // We cannot see the difference between any definition of `.none`.
     expect(Ray.none).toBe(Ray.none)
     expect(Ray.none).toEqual(Ray.none)
-    // // We can see the difference between each function definition.
+    // We cannot see the difference between any instantiation of `.none`.
+    expect(Ray.none()).toBe(Ray.none())
+    expect(Ray.initial()).toBe(Ray.initial())
+    expect(Ray.self()).toBe(Ray.self())
+    expect(Ray.terminal()).toBe(Ray.terminal())
+    // We CAN see the difference between a definition & instantiation of `.none`. - "They'd be different types"
+    expect(Ray.none).not.toBe(Ray.none())
+    expect(Ray.initial).not.toBe(Ray.initial())
+    expect(Ray.self).not.toBe(Ray.self())
+    expect(Ray.terminal).not.toBe(Ray.terminal())
+    // We can see the difference between each function definition.
     // expect(Ray.none).not.toBe(Ray.initial)
     // expect(Ray.none).not.toBe(Ray.self)
     // expect(Ray.none).not.toBe(Ray.terminal)
@@ -88,17 +69,6 @@ describe("ray", () => {
 
     // expect(Ray.none()).toBe(Ray.true)
 
-    // console.log(Ray.none())
-    // We cannot see the difference between any instantiation of `.none`.
-    // expect(Ray.none()).toBe(Ray.none())
-    // expect(Ray.initial()).toBe(Ray.initial())
-    // expect(Ray.self()).toBe(Ray.self())
-    // expect(Ray.terminal()).toBe(Ray.terminal())
-    // We CAN see the difference between a definition & instantiation of `.none`. - "They'd be different types"
-    // expect(Ray.none).not.toBe(Ray.none())
-    // expect(Ray.initial).not.toBe(Ray.initial())
-    // expect(Ray.self).not.toBe(Ray.self())
-    // expect(Ray.terminal).not.toBe(Ray.terminal())
 
     // let A = new Ray(false)
     // let B = new Ray(true)

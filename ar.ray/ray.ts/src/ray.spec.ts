2fc934c183a8203ea90b736770563b7a19baa77f
Fadi Shawki <fadi.shawki@orbitmines.com>
2024-07-10 14:01:09 +0200

    Thinking on implementation

--- ar.ray/ray.ts/src/ray.spec.ts
+++ ar.ray/ray.ts/src/ray.spec.ts
@@ -24,39 +24,55 @@
 //   }
 //
 
-import {__ray__} from "./ray";
-
-const Ray = __ray__({ DEBUG: true })
+import Ray from "./ray";
 
 describe("ray", () => {
   test("Minimal setup", () => {
-    // expect(Ray.none.is_none()).toBe(undefined)
+    const ray = new Ray([true, false, false, true, true]);
+    const A = new Ray(false)
+    const B = new Ray(true)
 
-    // expect(Ray.none).toBe('none')
+    // // expect(Ray).toBe(Ray.none)
+    // // expect(Ray()).toBe(Ray.none)
+    // console.log('a', Ray)
+    // // console.log(Ray())
+    // console.log('c', Ray.none)
     //
-    // We cannot see the difference between any definition of `.none`.
-    expect(Ray.none).toBe(Ray.none)
-    expect(Ray.none).toEqual(Ray.none)
-    // We cannot see the difference between any instantiation of `.none`.
-    expect(Ray.none()).toBe(Ray.none())
-    expect(Ray.initial()).toBe(Ray.initial())
-    expect(Ray.self()).toBe(Ray.self())
-    expect(Ray.terminal()).toBe(Ray.terminal())
-    // We CAN see the difference between a definition & instantiation of `.none`. - "They'd be different types"
-    expect(Ray.none).not.toBe(Ray.none())
-    // expect(Ray.initial).not.toBe(Ray.initial())
-    // expect(Ray.self).not.toBe(Ray.self())
-    // expect(Ray.terminal).not.toBe(Ray.terminal()) TODO
-    // We can see the difference between each function definition.
-    // expect(Ray.none).not.toBe(Ray.initial)
-    // expect(Ray.none).not.toBe(Ray.self)
-    // expect(Ray.none).not.toBe(Ray.terminal)
-    // expect(Ray.null).not.toBe(Ray.undefined)
-
-    // expect(Ray.none()).toBe(Ray.true)
-
-    // let A = new Ray(false)
-    // let B = new Ray(true)
+    // // We cannot see the difference between any definition of `.none`.
+    // expect(Ray.none).toBe(Ray.none)
+    // expect(Ray.none).toEqual(Ray.none)
+    //
+    // // We cannot see the difference between any instantiation of `.none`.
+    // expect(Ray.none()).toBe(Ray.none())
+    // expect(Ray.initial()).toBe(Ray.initial())
+    // expect(Ray.self()).toBe(Ray.self())
+    // expect(Ray.terminal()).toBe(Ray.terminal())
+    //
+    // //
+    // expect(Ray.none().is_none()).toBe(Ray.true)
+    //
+    // // TODO:
+    // // expect(Ray.none).toBe(Ray.none())
+    // // We CAN see the difference between a definition & instantiation of `.initial`. - "They'd be different types"
+    // // expect(Ray.initial).not.toBe(Ray.initial())
+    // // expect(Ray.self).not.toBe(Ray.self())
+    // // expect(Ray.terminal).not.toBe(Ray.terminal()) TODO
+    // // We can see the difference between each function definition.
+    // // expect(Ray.none).not.toBe(Ray.initial)
+    // // expect(Ray.none).not.toBe(Ray.self)
+    // // expect(Ray.none).not.toBe(Ray.terminal)
+    // // expect(Ray.null).not.toBe(Ray.undefined)
+    //
+    // // expect(Ray.none()).toBe(Ray.true)
+    //
+    // let A = new Ray(false); let B = new Ray(true)
+    // A.terminal = B
+    //
+    // console.log(A)
+    // console.log(B)
+    //
+    // expect(A()).toBe(B)
+    // expect(A()).not.toBe(A)
 
     // expect(A.compose(B)).toBe(A)
     // expect(A.next()).toBe(B)

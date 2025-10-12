24ad4a35653072ec245839c0877c6039f7b72ae1
Fadi Shawki <fadi.shawki@orbitmines.com>
2025-10-12 23:34:03 +0200

    Add possible dependencies

--- _download_dependencies.sh
+++ _download_dependencies.sh
@@ -41,3 +41,32 @@ cp -r ".orbitmines/external/github.com/googlefonts/noto-emoji/fonts/NotoColorEmo
 # TODO: Where's the emoji monochrome? Can manually download from here but there's no link/github repo. https://fonts.google.com/noto/specimen/Noto+Emoji
 #mkdir -p "$FONT_DIR/NotoEmoji/"
 
+
+
+# TODO Models
+# Download https://git-lfs.com/
+# git lfs install
+# git clone git@hf.co:meta-llama/Llama-4-Scout-17B-16E-Instruct
+# git clone git@hf.co:meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8
+# git clone git@hf.co:mistralai/Voxtral-Mini-3B-2507
+# etc... Allow any huggingface model configured somewhere. What's the default?
+
+# Install cmake, requirement for clang
+
+# clang, to use tinygrad clang
+# git clone git@github.com:llvm/llvm-project.git
+# cd llvm-project
+# mkdir build
+# cd build
+# cmake -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ../llvm
+# make
+
+# WebGPU runtime
+# sudo curl -L https://github.com/wpmed92/pydawn/releases/download/v0.3.0/libwebgpu_dawn_x86_64.so -o /usr/lib/libdawn.so
+# sudo curl -L https://github.com/wpmed92/pydawn/releases/download/v0.3.0/libwebgpu_dawn_x86_64.so -o /usr/lib/libwebgpu_dawn.so
+
+# git clone git@github.com:tinygrad/tinygrad.git
+# cd tinygrad
+# python3 -m venv .venv
+# source .venv/bin/activate
+# python3 -m pip install -e .

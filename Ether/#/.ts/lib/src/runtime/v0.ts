95708c90bd454a8afbb8cdbcdb5cafd8342a63bf
Fadi Shawki <fadi.shawki@orbitmines.com>
2025-11-23 16:22:05 +0100

    Further thoughts on functions and ray structure

--- Ether/#/.ts/lib/src/runtime/v0.ts
+++ Ether/#/.ts/lib/src/runtime/v0.ts
@@ -1,1 +1,2 @@
 //TODO = removes anything from the previous value from the object (so we need to know the origin of the things that set the vars)
+//TODO Keys are encoded on Edges on the Ray node definition.
